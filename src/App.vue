545b1a07d67adcd30417f89f4aa6918f30ea1890
Serg Kostyuk <serg.kostyuk@gmail.com>
2025-01-07 16:46:46 -0500

    First

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+
+const value = ref('')
+</script>
+
+<template>
+  <div class="text-3xl text-green-400 font-bold underline">IT SERG K</div>
+
+  <div class="card flex justify-center">
+    <Knob v-model="value" />
+  </div>
+
+  <div class="card flex flex-col items-center gap-4">
+    <div class="flex flex-wrap gap-4 justify-center">
+      <Button icon="pi pi-home" aria-label="Save" size="small" />
+      <Button label="Profile" icon="pi pi-user" size="small" />
+      <Button label="Save" icon="pi pi-check" iconPos="right" size="small" />
+    </div>
+    <div class="flex flex-wrap gap-4 justify-center">
+      <Button label="Search" icon="pi pi-search" iconPos="top" size="small" />
+      <Button label="Update" icon="pi pi-refresh" iconPos="bottom" />
+    </div>
+  </div>
+
+  <div class="card flex justify-center flex-wrap gap-4">
+    <Button label="Primary" size="large" />
+    <Button label="Secondary" severity="secondary" />
+    <Button label="Success" severity="success" />
+    <Button label="Info" severity="info" />
+    <Button label="Warn" severity="warn" />
+    <Button label="Help" severity="help" />
+    <Button label="Danger" severity="danger" />
+    <Button label="Contrast" severity="contrast" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './styles.scss'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import PrimeVue from 'primevue/config'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.use(PrimeVue, { theme: 'none' })
+
+app.mount('#app')
